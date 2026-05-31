a22b81bc839cce44e77eef5767cb45e3c6b03289
Pitfield Digital <hello@pitfielddigital.co.uk>
2026-05-31 14:00:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://pitfielddigital.github.io',
-  base: '/348-ilkeston-air-cadets',
+  site: 'https://ilkestonaircadets.com',
+  base: '/',
 });
